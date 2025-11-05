759ab818f1b0bbebf8f34879a835f8c6cd95eb5b
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2025-11-05 16:01:33 +0100

    creating file and adding app.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,6 +40,10 @@ function App() {
           <h2 className="block-primary-main">JS Monorepo</h2>
           <p className="block-primary-sub">Votre framework JavaScript</p>
         </hgroup>
+        <button type="button">
+          Commencer l'aventure
+        </button>
+        
         <p>Vous avez lu le README ?</p>
       </main>
 
