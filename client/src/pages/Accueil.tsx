aca3ce6e9e5ed9c7616e386e851f22c3d55e972f
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2025-11-07 11:34:28 +0100

    correcting stylizing toggle and modify app.tsx

--- client/src/pages/Accueil.tsx
+++ client/src/pages/Accueil.tsx
@@ -1,10 +1,7 @@
 import Avis from "../components/Avis";
 import CardTarifs from "../components/CardTarifs";
 import ExerciceVideo from "../components/ExerciceVideo";
-import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import Toggle from "../components/Toggle";
-
 
 function Accueil() {
   return (
@@ -13,8 +10,6 @@ function Accueil() {
       <ExerciceVideo />
       <CardTarifs />
       <Avis />
-      <Toggle />
-      <Footer />
     </>
   );
 }
