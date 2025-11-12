58f160950dad3815eb32e7be21540913fd9abee3
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2025-11-12 09:35:30 +0100

    adding change on form before pull from dev

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,12 @@
+import Formulaire from "../components/Formulaire";
+
 function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h1>About</h1>
+      <Formulaire />
+    </>
+  );
 }
 
 export default About;
