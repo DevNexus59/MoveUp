43364b0febbfed833a5b52f19a2a29d31c49c745
cecilepardo <pardo.cle@gmail.com>
2025-11-13 10:19:26 +0100

    search bar in Entrainements

--- client/src/pages/Entrainements.tsx
+++ client/src/pages/Entrainements.tsx
@@ -1,5 +1,22 @@
-function Entrainements() {
-  return <h1>Entraînements</h1>;
-}
+import React from 'react';
+import Search from '../components/Search';
+const Entrainements: React.FC = () => {
+  const exercices = [
+    'Pompes',
+    'Squats',
+    'Abdos',
+    'Fentes',
+    'Tractions',
+    'Planche',
+    'Burpees'
+  ];
+
+  return (
+    <div>
+      <h1>Liste des Entraînements</h1>
+      <Search data={exercices} />
+    </div>
+  );
+};
 
 export default Entrainements;
