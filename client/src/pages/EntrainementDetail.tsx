ea70c95eabb3d328493f1ff0fe5b9b5d0ecd53a8
aurelie_dumotier <aurelie.dumotier@gmail.com>
2025-11-24 12:15:24 +0100

    adding favorites to EntrainementDetail

--- client/src/pages/EntrainementDetail.tsx
+++ client/src/pages/EntrainementDetail.tsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
 import { useNavigate, useParams } from "react-router";
 import { Link } from "react-router";
+import { useAuth } from "../context/AuthContext";
 import ExercicesContext from "../context/ExercicesContext";
 import type { Exercice } from "../types/types";
 
+import etoilePleine from "../assets/etoile-pleine.png";
+import etoileVide from "../assets/etoile-vide.png";
 import chronometre from "../assets/images/entrainementdetail_time.png";
 
 import "./EntrainementDetail.css";
@@ -12,6 +15,8 @@ function EntrainementDetail() {
   const { id } = useParams();
   const navigate = useNavigate();
   const context = useContext(ExercicesContext);
+
+  const { user, userId, setUser } = useAuth();
 
   if (!context) {
     return <p>Erreur : contexte non disponible</p>;
@@ -29,6 +34,36 @@ function EntrainementDetail() {
     return <p>Exercice non trouvé</p>;
   }
 
+  const isFavorite =
+    user?.favoriteExercices?.includes(String(exercice.exerciseId)) || false;
+
+  const handleToggleFavorite = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:4000/api/${userId}/favorites`,
+        {
+          method: "PATCH",
+          headers: {
+            "content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            exerciseId: exercice.exerciseId,
+          }),
+        },
+      );
+
+      if (!response.ok) {
+        console.error("Échec de la mise à jour du favori");
+        return;
+      }
+
+      const updateUser = await response.json();
+      setUser(updateUser);
+    } catch (error) {
+      console.error("Erreur réseau :", error);
+    }
+  };
+
   return (
     <div className="entrainement-detail-container">
       <button
@@ -38,7 +73,20 @@ function EntrainementDetail() {
       >
         ← Retour
       </button>
-      <h1 className="entrainement-detail-title">{exercice.nom}</h1>
+      <section className="entrainement-detail-section-title">
+        <h1 className="entrainement-detail-title">{exercice.nom}</h1>
+        <button
+          type="button"
+          className="entrainement-detail-favoris-btn"
+          onClick={handleToggleFavorite}
+        >
+          <img
+            src={isFavorite ? etoilePleine : etoileVide}
+            alt="étoile favoris"
+            className="entrainement-detail-favoris-icon"
+          />
+        </button>
+      </section>
       <section className="entrainement-detail-card">
         <div className="entrainement-detail-informations">
           <div className="entrainement-detail-difficulte">
